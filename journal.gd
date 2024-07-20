c349769ea98dae8ce819d4e792b3ac2d9071aa17
knightofiron <byubatman13@gmail.com>
2024-07-20 09:20:58 -0500

    Continued Journal UI
    It's better now, at least.

---
+++ journal.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _on_left_arrow_pressed():
+	print("Journal: Left Arrow")
+
+func _on_right_arrow_pressed():
+	print("Journal: Right Arrow")
+
+# I assume we need some kind of page system here which determines the displayed text.
