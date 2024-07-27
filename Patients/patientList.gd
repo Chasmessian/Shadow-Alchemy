6fc237830bde12f02fdebdd729c62159a5b5781b
Chasmessian <evantylerlarson@gmail.com>
2024-07-26 21:52:04 -0500

    stuff

--- Patients/patientList.gd
+++ Patients/patientList.gd
@@ -4,7 +4,6 @@ static var patients = ["res://Patients/List/Patient1/patient1.gd", "res://Patien
 static func getPatient():
 	if(len(patients)==0):
 		return null
-	print(len(patients)-1)
 	var randNum = randi_range(0,len(patients)-1)
 	#print(randNum)
 	return patients.pop_at(randNum)
