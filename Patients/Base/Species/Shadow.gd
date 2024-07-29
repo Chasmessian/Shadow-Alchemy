dd3cd2596cbc712ac598ad98bb6fdfd1b482bae5
Chasmessian <evantylerlarson@gmail.com>
2024-07-29 12:15:08 -0500

    fixed tutorial bug

--- Patients/Base/Species/Shadow.gd
+++ Patients/Base/Species/Shadow.gd
@@ -2,6 +2,6 @@ class_name Shadow
 extends Species
 static var vocals = preload("res://UI/Garden/Sounds/watering/watering3.wav")
 static func ratePotion(potion):
-	return 0
+	return [0, false]
 static func attemptSwap(ingredient):
 	pass
